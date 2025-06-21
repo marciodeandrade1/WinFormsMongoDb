f59ff1d464ecfb023720a002f77f2f22d43504ba
gmand <gmand@DESKTOP-2KO8PEB>
2025-06-21 09:54:37 -0400

    Criação de pasta e classe Model Usuario

---
+++ WinFormsMongoDb/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+
+namespace WinFormsMongoDb.Models
+{
+    public class Usuario
+    {
+        public ObjectId Id { get; set; } // campo obrigatório para o MongoDb
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
